f3ad1ffe4685baed0dfa037a5bd13407d441c7ac
abdul-nishar <sheikhabdul285@gmail.com>
2024-04-07 04:27:03 +0530

    Professor's Page Done-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
 
 // import LoginPage from "./pages/loginPage";
-import DepartmentPage from "./pages/departmentPage";
+// import DepartmentPage from "./pages/departmentPage";
+import ProfessorPage from "./pages/professorPage";
 import { modalTheme } from "./themes/modalTheme";
 import { buttonTheme } from "./themes/buttonTheme";
 import "./App.css";
@@ -14,7 +15,8 @@ function App() {
   return (
     <ChakraBaseProvider theme={theme}>
       {/* <LoginPage></LoginPage> */}
-      <DepartmentPage></DepartmentPage>
+      {/* <DepartmentPage></DepartmentPage> */}
+      <ProfessorPage></ProfessorPage>
     </ChakraBaseProvider>
   );
 }
